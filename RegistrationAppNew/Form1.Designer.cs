cb868c78bfb5f595c8fc4a8a2b776728363442fc
Annie Embrink <anisan-1@student.ltu.se>
2022-09-19 11:45:10 +0200

    Add project files.

---
+++ RegistrationAppNew/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace RegistrationAppNew
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstNameInput = new System.Windows.Forms.TextBox();
+            this.lastNameInput = new System.Windows.Forms.TextBox();
+            this.idInput = new System.Windows.Forms.TextBox();
+            this.resultBox = new System.Windows.Forms.TextBox();
+            this.submitBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.newPersonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // firstNameInput
+            // 
+            this.firstNameInput.Location = new System.Drawing.Point(200, 36);
+            this.firstNameInput.Name = "firstNameInput";
+            this.firstNameInput.Size = new System.Drawing.Size(100, 20);
+            this.firstNameInput.TabIndex = 3;
+            // 
+            // lastNameInput
+            // 
+            this.lastNameInput.Location = new System.Drawing.Point(200, 62);
+            this.lastNameInput.Name = "lastNameInput";
+            this.lastNameInput.Size = new System.Drawing.Size(100, 20);
+            this.lastNameInput.TabIndex = 4;
+            // 
+            // idInput
+            // 
+            this.idInput.Location = new System.Drawing.Point(200, 88);
+            this.idInput.Name = "idInput";
+            this.idInput.Size = new System.Drawing.Size(100, 20);
+            this.idInput.TabIndex = 5;
+            // 
+            // resultBox
+            // 
+            this.resultBox.Font = new System.Drawing.Font("Monospac821 BT", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultBox.Location = new System.Drawing.Point(326, 36);
+            this.resultBox.Multiline = true;
+            this.resultBox.Name = "resultBox";
+            this.resultBox.Size = new System.Drawing.Size(180, 72);
+            this.resultBox.TabIndex = 6;
+            // 
+            // submitBtn
+            // 
+            this.submitBtn.BackColor = System.Drawing.Color.Transparent;
+            this.submitBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.submitBtn.Font = new System.Drawing.Font("Monospac821 BT", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submitBtn.Location = new System.Drawing.Point(214, 116);
+            this.submitBtn.Name = "submitBtn";
+            this.submitBtn.Size = new System.Drawing.Size(86, 31);
+            this.submitBtn.TabIndex = 7;
+            this.submitBtn.Text = "Submit";
+            this.submitBtn.UseVisualStyleBackColor = false;
+            this.submitBtn.Click += new System.EventHandler(this.submitBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monospac821 BT", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(117, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 14);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "First name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Monospac821 BT", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(124, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 14);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Last name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Monospac821 BT", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(82, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 14);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Id (YYMMDDXXXX)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Monospac821 BT", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(323, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 14);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Result";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.newPersonToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 13;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(38, 20);
+            this.toolStripMenuItem1.Text = "Exit";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // newPersonToolStripMenuItem
+            // 
+            this.newPersonToolStripMenuItem.Name = "newPersonToolStripMenuItem";
+            this.newPersonToolStripMenuItem.Size = new System.Drawing.Size(82, 20);
+            this.newPersonToolStripMenuItem.Text = "New Person";
+            this.newPersonToolStripMenuItem.Click += new System.EventHandler(this.newPersonToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.resultBox);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.firstNameInput);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lastNameInput);
+            this.panel1.Controls.Add(this.idInput);
+            this.panel1.Controls.Add(this.submitBtn);
+            this.panel1.Location = new System.Drawing.Point(209, 176);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(544, 168);
+            this.panel1.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.submitBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox firstNameInput;
+        private System.Windows.Forms.TextBox lastNameInput;
+        private System.Windows.Forms.TextBox idInput;
+        private System.Windows.Forms.TextBox resultBox;
+        private System.Windows.Forms.Button submitBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem newPersonToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ RegistrationAppNew/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace RegistrationAppNew
+{
+    public partial class Form1 : Form
+    {
+
+        //Creating a class
+        public class Person
+        {
+            public string firstname;
+            public string lastname;
+            public string id;
+            public string sex;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            panel1.Visible = false;
+        }
+
+        static bool InputIsFilled(string firstname, string lastname, string id)
+        {
+            //Check if fields are filled
+            if (firstname == "" || lastname == "" || id == "")
+            {
+                //If not, tell the user to do so
+                MessageBox.Show("All fields have to be filled");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        static bool CorrectFormatName(string name)
+        {
+            if (!System.Text.RegularExpressions.Regex.IsMatch(name, @"^[a-zA-Z]+$"))
+            {
+                MessageBox.Show($"Firstname and lastname: Only letters allowed");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        static bool CorrectFormatId(string id)
+        {
+            if (!System.Text.RegularExpressions.Regex.IsMatch(id, "^[0-9]*$"))
+            {
+                MessageBox.Show($"ID: Only numbers allowed");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        static bool AllFormatOk(string firstname, string lastname, string id)
+        {
+            if (CorrectFormatName(firstname) == false || CorrectFormatName(lastname) == false || CorrectFormatId(id) == false)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        static bool CorrectIdLenght(string id)
+        {
+            //Check that id consists of ten numbers
+            if (id.Length != 10)
+            {
+                MessageBox.Show("ID number has to be 10 numbers, format YYMMDDXXXX");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        static string MaleOrFemale(string id)
+        {
+            string str;
+
+            //Check if its male or female
+            if (id[8] % 2 == 0)
+            {
+                str = "female";
+            }
+            else
+            {
+                str = "male";
+            }
+            return str;
+        }
+
+        static void SeparateId(List<int> list, List<int> list2)
+        {
+            foreach (int nr in list)
+            {
+                //if nr is more than one nr e.g. 18...
+                if (nr.ToString().Length > 1)
+                {
+                    //Separate it (18 --> 1, 8)
+                    for (int i = 0; i < nr.ToString().Length; i++)
+                    {
+                        char nr2 = nr.ToString()[i];
+
+                        //Add it to the new list
+                        list2.Add(int.Parse(nr2.ToString()));
+                    }
+                }
+                else
+                {
+                    list2.Add(nr);
+                }
+            }
+        }
+
+        static bool TotalOfNumbers(List<int> list2)
+        {
+            //Sum up all numbers in the list
+            var total = 0;
+            foreach (int item in list2)
+            {
+                total += item;
+            }
+
+            //Check if total is evenly divisible with 10
+            if (total % 10 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("The ID number is not correct, try again");
+                return false;
+            }
+        }
+        static bool CorrectId(string id)
+        {
+            List<int> list = new List<int>();
+            List<int> list2 = new List<int>();
+
+            //For every char in the string...
+            for (int i = 0; i < id.Length; i++)
+            {
+                //Convert char into int (so we can count on it later)
+                int nr = int.Parse(id[i].ToString());
+
+                //If index in the loop is divisible with two (i = 0, 2, 4 etc) then add to list
+                if (i % 2 == 0)
+                {
+                    nr = nr * 2;
+                    list.Add(nr);
+                }
+                //same but if i is divisible with one
+                else
+                {
+                    nr = nr * 1;
+                    list.Add(nr);
+                }
+            }
+            SeparateId(list, list2);
+
+            if (TotalOfNumbers(list2) == false)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private void submitBtn_Click(object sender, EventArgs e)
+        {
+            //When submitbutton is clicked
+            //Clear resultbox
+            resultBox.Clear();
+
+            //Firstname and lastname should have inital letter uppercase and rest of string lowercase
+            firstNameInput.Text = char.ToUpper(firstNameInput.Text[0]) + firstNameInput.Text.Substring(1).ToLower();
+            lastNameInput.Text = char.ToUpper(lastNameInput.Text[0]) + lastNameInput.Text.Substring(1).ToLower();
+
+            //Check if all input fields are filled in
+            if (InputIsFilled(firstNameInput.Text, lastNameInput.Text, idInput.Text))
+            {
+                //Check if format is ok (name should be letters and id should be numbers)
+                if (AllFormatOk(firstNameInput.Text, lastNameInput.Text, idInput.Text))
+                {
+                    //Check if id is 10 numbers
+                    if (CorrectIdLenght(idInput.Text))
+                    {
+                        //Check if id is correct
+                        if (CorrectId(idInput.Text))
+                        {
+                            //If all fields are filled and have right format, create a new object, a person
+                            Person p1 = new Person();
+                            p1.firstname = firstNameInput.Text;
+                            p1.lastname = lastNameInput.Text;
+                            p1.id = idInput.Text;
+
+                            //If everything is fine, write the result in the resultbox
+                            resultBox.Text += ($"Name: {p1.firstname} {p1.lastname}\r\n");
+                            resultBox.Text += ($"ID: {p1.id}\r\n");
+                            resultBox.Text += ($"Sex: {MaleOrFemale(idInput.Text)}");
+
+                            //When result is displayed, clear all input fields
+                            firstNameInput.Clear();
+                            lastNameInput.Clear();
+                            idInput.Clear();
+                        }
+                    }
+                }
+            }
+        }
+        //Menu
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void newPersonToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Display all fields
+            panel1.Visible = true;
+
+            //Clear all fields
+            resultBox.Clear();
+            firstNameInput.Clear();
+            lastNameInput.Clear();
+            idInput.Clear();
+        }
+    }
+}
+
